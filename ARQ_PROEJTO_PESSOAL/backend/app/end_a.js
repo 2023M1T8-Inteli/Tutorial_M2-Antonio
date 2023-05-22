28147b40107c5269aa219422888c50e99a32ae71
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2023-05-22 13:59:39 -0300

    Atualizacao endpoints + mapa int

--- ARQ_PROEJTO_PESSOAL/backend/app/end_a.js
+++ ARQ_PROEJTO_PESSOAL/backend/app/end_a.js
@@ -1,9 +1,11 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const sqlite3 = require('sqlite3').verbose();
-const DBPATH = '/Users/macbook/Documents/GitHub/Prsnl/backend/src/database/banco_de_dados_ipt_grupo02.db';
+const DBPATH = '/Users/macbook/Documents/GitHub/Tutorial_M2-Antonio/ARQ_PROEJTO_PESSOAL/backend/src/database/banco_de_dados_ipt_grupo02att.db';
 
 const hostname = '127.0.0.1';
 const port = 3000
@@ -55,7 +57,7 @@ app.get('/projeto/picos', (req,res) => {
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
     var db = new sqlite3.Database(DBPATH);
-    var sql = `SELECT * FROM Pico WHERE viagem_id=${req.body.viagem_id}`;
+    var sql = `SELECT * FROM Pico WHERE viagem_id=${req.body.id_vagao}`;
     db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
@@ -69,12 +71,26 @@ app.get('/projeto/choques', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
     var db = new sqlite3.Database(DBPATH);
-    var sql = `SELECT * FROM Choque WHERE viagem_id=${req.body.viagem_id}`;
+    var sql = `SELECT * FROM Choque WHERE id_vagao=${req.body.id_vagao}`;
     db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
         }
         res.json(rows)
+    });
+    db.close();
+});
+
+app.get('/projeto/viagem', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = `SELECT * FROM Viagem WHERE id_viagem=${req.body.id_viagem}`
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
     });
     db.close();
 });
@@ -93,6 +109,39 @@ app.get('/projeto/comparacao', (req, res) => {
         res.json(rows);
     });
     db.close();
+});
+
+// Endpoints página inicial
+
+app.get('/meus-projetos', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = `SELECT * FROM Relatorio`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close();
+});
+
+// Endpoints mapa interativo
+
+app.get('/projeto/mapa', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = `SELECT choque.latitude, choque.longitude, choque.id, pico.latitude, pico.longitude, pico.id FROM choque, pico WHERE choque.id_vagao=${req.body.id_vagao}`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        };
+        res.json(rows);
+    });
+    db.close();
+    
 });
 
 
